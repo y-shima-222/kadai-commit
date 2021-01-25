662e4a6b3886e2c2febda7b41240df4fc2e99247
y.shima <ubuntu@ip-172-31-8-139.ap-northeast-1.compute.internal>
2021-01-25 13:51:52 +0000

    update sample.php

--- sample.php
+++ sample.php
@@ -1,11 +1,17 @@
+<?php
+    $result = '無し';
+?>
 <!DOCTYPE html>
 <html lang="ja">
     <head>
-        <meta charset="utf-8">
-        <title>Gitチュートリアル</title>
+        <meta charset="UTF-8">
+        <title>おみくじ</title>
     </head>
     <body>
-        <h1>はじめてのバージョン管理</h1>
-        <p>Gitを使ってバージョン管理する。</p>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
+        <form action="sample.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじをひく！</button>
+        </form>
     </body>
 </html>
